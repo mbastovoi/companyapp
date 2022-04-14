9a71e5f31feb83932fbe98d5b85ba7c7f37eba7c
Mircea Bastovoi <mircea.bastovoi@pixieset.com>
2022-04-14 18:11:43 +0300

    A function to get a company name where the employee works

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    public function company()
+    {
+        $company = Company::where('id', $this->company_id)->value('name');
+        return $company;
+    }
 }
