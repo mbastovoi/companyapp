66552357b1556070ef96dab27d29d59e1f4682ea
Mircea Bastovoi <mircea.bastovoi@pixieset.com>
2022-04-14 18:16:59 +0300

    Web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\SessionsController;
+use App\Http\Controllers\AdminCompanyController;
+use App\Http\Controllers\AdminEmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [CompanyController::class, 'index']);
+
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest')->name('login');
+Route::post('login', [SessionsController::class, 'login']);
+
+
+Route::get('register', [UserController::class, 'create'])->middleware('guest')->name('register');
+Route::post('register', [UserController::class, 'store'])->middleware('guest');
+Route::get('logout', [UserController::class, 'logout']);
+
+//Admin routes
+
+Route::get('/admin/companies/create', [AdminCompanyController::class, 'create'])->middleware('admin');
+Route::post('/admin/companies/create', [AdminCompanyController::class, 'store'])->middleware('admin');
+Route::get('/admin/companies/', [AdminCompanyController::class, 'show'])->middleware('admin');
+Route::patch('/admin/companies/{company}', [AdminCompanyController::class, 'update'])->middleware('admin');
+Route::delete('/admin/companies/{company}', [AdminCompanyController::class, 'destroy'])->middleware('admin');
+
+Route::get('/admin/employees/create', [AdminEmployeeController::class, 'create'])->middleware('admin');
+Route::post('/admin/employees/create', [AdminEmployeeController::class, 'store'])->middleware('admin');
+Route::get('/admin/employees/', [AdminEmployeeController::class, 'show'])->middleware('admin');
+Route::get('/admin/employees/edit/{employee}', [AdminEmployeeController::class, 'edit'])->middleware('admin');
+Route::patch('/admin/employees/edit/{employee}', [AdminEmployeeController::class, 'update'])->middleware('admin');
+Route::delete('/admin/employees/{employee}', [AdminEmployeeController::class, 'destroy'])->middleware('admin');
